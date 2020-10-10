f003b5370a1b781b78b005db3dc738c12e278353
Mafrans <malte.kluft@gmail.com>
2020-10-10 23:34:19 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,107 @@
+<template
+    ><q-layout view="lhr LpR fFf">
+        <q-header bordered class="bg-primary text-white" height-hint="98">
+            <q-toolbar>
+                <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
+
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img
+                            src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
+                        />
+                    </q-avatar>
+                    Mafrans's Manga Translator
+                </q-toolbar-title>
+
+                <q-btn dense flat round icon="menu" @click="rightDrawerOpen = !rightDrawerOpen" />
+            </q-toolbar>
+
+            <q-tabs align="left">
+                <q-route-tab to="/page1" label="Page One" />
+                <q-route-tab to="/page2" label="Page Two" />
+                <q-route-tab to="/page3" label="Page Three" />
+            </q-tabs>
+        </q-header>
+
+        <q-drawer
+            show-if-above
+            v-model="leftDrawerOpen"
+            side="left"
+            behavior="desktop"
+            bordered
+        >
+            <!-- drawer content -->
+        </q-drawer>
+
+        <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+            <!-- drawer content -->
+        </q-drawer>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script lang="ts">
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksData = [
+    {
+        title: 'Docs',
+        caption: 'quasar.dev',
+        icon: 'school',
+        link: 'https://quasar.dev',
+    },
+    {
+        title: 'Github',
+        caption: 'github.com/quasarframework',
+        icon: 'code',
+        link: 'https://github.com/quasarframework',
+    },
+    {
+        title: 'Discord Chat Channel',
+        caption: 'chat.quasar.dev',
+        icon: 'chat',
+        link: 'https://chat.quasar.dev',
+    },
+    {
+        title: 'Forum',
+        caption: 'forum.quasar.dev',
+        icon: 'record_voice_over',
+        link: 'https://forum.quasar.dev',
+    },
+    {
+        title: 'Twitter',
+        caption: '@quasarframework',
+        icon: 'rss_feed',
+        link: 'https://twitter.quasar.dev',
+    },
+    {
+        title: 'Facebook',
+        caption: '@QuasarFramework',
+        icon: 'public',
+        link: 'https://facebook.quasar.dev',
+    },
+    {
+        title: 'Quasar Awesome',
+        caption: 'Community Quasar projects',
+        icon: 'favorite',
+        link: 'https://awesome.quasar.dev',
+    },
+];
+
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+    name: 'MainLayout',
+    components: { EssentialLink },
+    setup() {
+        const leftDrawerOpen = ref(false);
+        const rightDrawerOpen = ref(false);
+        const essentialLinks = ref(linksData);
+
+        return { leftDrawerOpen, rightDrawerOpen, essentialLinks };
+    },
+});
+</script>
